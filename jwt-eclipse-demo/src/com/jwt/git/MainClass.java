2d0d8dad43decc779ca47595426dfd12333f45c1
Aakash Soni <aakashsoni1209@gmail.com>
2022-05-01 18:49:22 +0530

    First commit

--- jwt-eclipse-demo/src/com/jwt/git/MainClass.java
+++ jwt-eclipse-demo/src/com/jwt/git/MainClass.java
@@ -3,7 +3,7 @@ package com.jwt.git;
 public class MainClass {
 
 	public static void main(String[] args){
-		System.out.println("Hello there!");
+		System.out.println("Done with integration");
 	}
 	
 }
